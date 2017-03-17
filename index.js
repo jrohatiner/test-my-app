a28075508e6fa97e0a9d16419f5a228103d879ad
jr <judithrohatiner@gmail.com>
2017-03-17 10:42:38 -0400

    updates
    Updated code

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+//app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+
+var engines = require('consolidate');
+
+app.engine(‘ejs’, engines.ejs);
+
+app.engine('handlebars', engines.handlebars);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+app.get('/', function(request, response) {
+  response.render('pages/about')
+});
+app.get('/', function(request, response) {
+  response.render('pages/faq')
+});
+app.get('/', function(request, response) {
+  response.render('pages/contact')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
